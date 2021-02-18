65939f4b0ac6f60a1856798fdce1f86faffcf748
MokshaTecno <78843844+MokshaTecno@users.noreply.github.com>
2021-02-18 15:00:41 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class Employee {
+	public static void main(String[] args) {
+		int id=10;
+		int salary=4000;
+		String name="moksha";
+		System.out.println(id);
+		System.out.println(salary);
+		System.out.println(name);
+	}
+
+}
---
+++ helloword.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class helloword {
+
+	public static void main(String[] args) {
+		System.out.println("helloword");
+		// TODO Auto-generated method stub
+
+	}
+
+}
